97f546bd8179f0d6150a3459cb0dd9e292e0b113
DjordyKoert <djordy.koert@live.nl>
2018-09-20 20:29:49 +0200

    show server name in DB

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ bot.on("guildDelete", guid => {
 })
 //DB error
 con.connect(err => {
-  if (err) { return createLog(fs, err); }
+  if (err) { let errstack = err.stack; createLog(fs, err, errstack); return; }
   console.log("Connected to database");
 });
 
@@ -35,7 +35,7 @@ con.connect(err => {
 bot.on("message", async message => {
   //Check if member is in database
   con.query(`SELECT * FROM Test WHERE id='${message.author.id}'`, (err, rows) => {
-    if (err) { return createLog(fs, err); }
+    if (err) { let errstack = err.stack; createLog(fs, err, errstack); return; }
     let sql;
     //If member not in database add member
     if (rows.length < 1) {
@@ -59,7 +59,7 @@ bot.on("message", async message => {
           //Update Status
           botconfig.status = "enabled";
           fs.writeFile("./botconfig.json", JSON.stringify(botconfig), function (err) {
-            if (err) { return createLog(fs, err); }
+            if (err) { let errstack = err.stack; createLog(fs, err, errstack); return; }
           });
           message.author.send("Bot Enabled.");
           console.log("Bot Enabled.");
@@ -73,7 +73,7 @@ bot.on("message", async message => {
           //Update Status
           botconfig.status = "disabled";
           fs.writeFile("./botconfig.json", JSON.stringify(botconfig), function (err) {
-            if (err) { return createLog(fs, err); }
+            if (err) { let errstack = err.stack; createLog(fs, err, errstack); return; }
           });
           message.author.send("Bot Disabled.");
           console.log("Bot Disabled");
@@ -87,7 +87,7 @@ bot.on("message", async message => {
           let txtmessage = args.slice(0).join(' ');
           if (txtmessage == "") { message.react("❌"); message.reply("Empty announcement message"); return; }
           con.query(`SELECT * FROM ssetup`, (err, rows) => {
-            if (err) { return createLog(fs, err); }
+            if (err) { let errstack = err.stack; createLog(fs, err, errstack); return; }
             let i = 0;
             whileLoop();
             function whileLoop() {
@@ -98,8 +98,8 @@ bot.on("message", async message => {
                 console.log(getAnnouncementID);
                 try {
                   bot.guilds.get(getServerID).channels.get(getAnnouncementID).send(`${txtmessage}`);
-                } catch (err) {
-                  if (err) { let extraMessage = "Bot probably left server and can't send an announcement. Leaving server..."; createLog(fs, err, extraMessage); }
+                } catch (err) { //If bot can't reach the server. AKA bot left the server
+                  if (err) { let errstack = err.stack; let extraMessage = "Bot probably left server and can't send an announcement. Leaving server..."; createLog(fs, err, errstack, extraMessage); }
                   sql = `DELETE FROM ssetup WHERE serverID='${getServerID}'`;
                   con.query(sql);
                 }
@@ -171,6 +171,7 @@ bot.on("message", async message => {
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   let server = message.guild.id;
+  let serverName = message.guild.name;
   //Command checker
   switch (command) {
     //Hoi Command
@@ -263,13 +264,13 @@ bot.on("message", async message => {
             let sql;
             //If server doesn't already have an announcement channel
             if (rows.length == 0) {
-              sql = `INSERT INTO ssetup (serverID, announcementID) VALUES ('${server}', '${announcementID.id}')`;
+              sql = `INSERT INTO ssetup (serverID, serverName,announcementID) VALUES ('${server}', '${serverName}','${announcementID.id}')`;
               con.query(sql);
               message.react("✅");
               console.log(`New annoucement channel in server: ${server}, announcementID=${announcementID}`)
             } //If server already has an announcement channel
             else {
-              sql = `UPDATE ssetup SET announcementID='${announcementID.id}' WHERE serverID='${server}'`;
+              sql = `UPDATE ssetup SET announcementID='${announcementID.id}', serverName='${serverName}'WHERE serverID='${server}'`;
               con.query(sql);
               message.react("✅");
               console.log(`Updated annoucement channel in server: ${server}, announcementID=${announcementID}`)
@@ -311,17 +312,13 @@ bot.on("message", async message => {
       });
       message.react("✅");
       break;
-      case "test":
-        let err="test err";
-        createLog(fs,err);
-      break;
     default: message.reply(`Unrecognized command use >help for a list of commands.`); message.react("❌");
   }
 });
 bot.login(botconfig.token);
 
 //Create an errorLog
-function createLog(fs, err, extraMessage) {
+function createLog(fs, err, errstack, extraMessage) {
   if (!extraMessage) extraMessage = "";
 
   let date = new Date();
@@ -338,7 +335,7 @@ function createLog(fs, err, extraMessage) {
     console.log("Creating ./logs folder");
   }
 
-  fs.writeFile(`./logs/err_${error_date}.txt`, `${err}\n\n${extraMessage}`, { flag: 'w' }, function (err) {
+  fs.writeFile(`./logs/err_${error_date}.txt`, `${err}\n${errstack}\n\n${extraMessage}`, { flag: 'w' }, function (err) {
     if (err) return console.error(err);
     console.log(`Succefully made logs file: err_${error_date}.txt`);
   });
