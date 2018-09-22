38ad1f96ad55e506d6c690e6ac05a0134adb9604
DjordyKoert <djordy.koert@live.nl>
2018-09-22 19:13:11 +0200

    owner_id

--- cmds/say.js
+++ cmds/say.js
@@ -1,6 +1,6 @@
 module.exports.run = async (bot, botconfig, fs, message, args, con, server) => {
     if (botconfig.status == "disabled") return message.reply("Bot is disabled by developer");
-    if (!message.member.hasPermission("ADMINISTRATOR")) { message.reply("No permission to use this command"); message.react("❌"); return; }
+    if (!message.member.hasPermission("ADMINISTRATOR") && !message.author.id == botconfig.Owner_id) { message.reply("No permission to use this command"); message.react("❌"); return; }
     let txtmessage = args.slice(0).join(' ');
     if (txtmessage == "") { message.react("❌"); message.reply("Empty message"); return; }
     message.delete();
