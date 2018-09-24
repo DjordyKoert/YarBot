8ec716b7a784f50792e716011ecc1d8dd0541428
DjordyKoert <33036133+DjordyKoert@users.noreply.github.com>
2018-09-24 10:11:04 +0200

    heroku removed, HEROKU PAS DOEN ALS BOT HELEMAAL KLAAR IS!

--- cmds/announcement.js
+++ cmds/announcement.js
@@ -1,8 +1,8 @@
-module.exports.run = async (bot, /* botconfig,*/ fs, message, args, con, server) => {
+module.exports.run = async (bot, botconfig, fs, message, args, con, server) => {
     if (message.author.id == botconfig.Owner_id) {
         let txtmessage = args.slice(0).join(' ');
         if (txtmessage == "") { message.react("❌"); message.reply("Empty announcement message"); return; }
-        console.log('\x1b[42m%s\x1b[0m: ',"Starting announcement");
+        console.log('\x1b[42m%s\x1b[0m: ', "Starting announcement");
         con.query(`SELECT * FROM ssetup WHERE announcementID !=""`, (err, rows) => {
             if (err) { let errstack = err.stack; createLog(fs, err, errstack); return; }
             let i = 0;
@@ -11,8 +11,8 @@ module.exports.run = async (bot, /* botconfig,*/ fs, message, args, con, server)
                 setTimeout(function () {
                     var getServerID = rows[i].serverID;
                     var getAnnouncementID = rows[i].announcementID;
-                    console.log("\x1b[32m",getServerID);
-                    console.log("\x1b[35m",getAnnouncementID);
+                    console.log("\x1b[32m", getServerID);
+                    console.log("\x1b[35m", getAnnouncementID);
                     try {
                         bot.guilds.get(getServerID).channels.get(getAnnouncementID).send(`${txtmessage}`);
                     } catch (err) { //If bot can't reach the server. AKA bot left the server
